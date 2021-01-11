c4b066f70760aec14ec5bf9a2aa2d0b679b8c9da
Medusa <63045112+Nandhini311@users.noreply.github.com>
2021-01-11 18:09:55 +0530

    Create preceptron.py

---
+++ Neural Network/Perceptron/preceptron.py
@@ -0,0 +1,37 @@
+import numpy as np
+
+class Perception:
+    
+    def __init__(self, learning_rate=0.01, n_iters=1000):
+        self.lr = learning_rate
+        self.n_iters = n_iters
+        self.activation_func = self._unit_step_func
+        self.weights = None
+        self.bias = None
+        
+    def fit(self, X, y):
+        n_samples, n_features = X.shape
+        
+        #init weights
+        self.weights = np.zeros(n_features)
+        self.bias = 0
+    
+        y_ = np.array([1 if i > 0 else 0 for i in y])
+        
+        for _ in range(self.iters):                                          //to find the best fit line 
+            for idx, x_i in enumerate(X):
+                linear_output = np.dot(x_i, self.weights)
+                y_predicted = activation_func(linear_output)
+                
+                update = (y_[idx] - y_predicted)
+                self.weights += update*x_i
+                self.bias += update
+                
+                
+    def predict(self, X):                                                    //to predict the 'y'
+        linear_output = np.dot(X,self.weights)+self.bias
+        y_predicted = self.activation_func(linear_output)
+        return y_predicted
+    
+    def __unit_step_func(self, X):
+        return np.where(x>=0,1,0)
